4ff33115a19386f087f29e183a1b71920d842ae3
Nima <nimacodes@gmail.com>
2024-02-11 19:14:48 +0330

    :rotating_light: BETA RELEASE FOR TESTS | Last Year's code deleted

--- src/index.requirements.js
+++
@@ -1,7 +0,0 @@
-const { files, folders, files2, folders2 } = require("./foldersAndFiles");
-const { createFilesAndFolders, installPackages, helpMessage, initGit, runServer, addDevCommandToPackageJson } = require("./topLevels");
-const clc = require('cli-color');
-const { execSync } = require("child_process");
-module.exports = {
-    files, folders, files2, folders2, createFilesAndFolders, clc, execSync,helpMessage, initGit, runServer, addDevCommandToPackageJson, installPackages
-}
