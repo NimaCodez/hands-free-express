d930e3a3a8b480d8c35127d59beca4928d735b52
Nima <nimacodes@gmail.com>
2024-02-12 18:03:36 +0330

    :construction: WINDOWS & LINUx TESTS PASSED :white_check_mark:

--- bin/install-deps.js
+++ bin/install-deps.js
@@ -4,8 +4,7 @@ import { execSync } from 'child_process';
 export function installDeps(os, packages) {
     for (let pkg of packages) {
 		console.log(chalk.cyan(`>> Installing [${pkg}] ...`));
-		if (os != 'win32') execSync(`sudo npm i ${pkg}`);
-		else execSync(`npm i ${pkg}`);
+		execSync(`npm i ${pkg}`);
 		console.log(chalk.green(`>+ INSTALLED PKG [${pkg}]`));
 	}
 }
